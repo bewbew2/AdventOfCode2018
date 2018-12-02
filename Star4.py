f495f44d5abb8575fe28fd3e6425d7aeb929233f
bewbew2 <paul.yaklin@gmail.com>
2018-12-02 16:36:09 -0600

    First upload

--- Star4.py
+++ Star4.py
@@ -4,32 +4,28 @@ with open(u'star3_input.txt', 'r') as f:
     box_ids = f.readlines()
 
 
-def mcount(_id):
-    d = []
-    t = []
-    for letter in set(_id):
-        n = _id.count(letter)
-        if n == 2:
-            d.append(n)
-        if n == 3:
-            t.append(n)
-    return len(set(d)),len(set(t))
+def mcount(l_d):
+    rep_count = 0
+    for item in l_d:
+        if re.match('[-+]', item):
+            rep_count += 1
+#    print rep_count
+
+    if rep_count < 5:
+        if rep_count > 0:
+            return True
+    else:
+        return False
 
 
-for i in range(len(box_ids)-1):
-    for j in range(len(box_ids[i+1:])):
+for i in range(len(box_ids)):
+    for j in range(len(box_ids[i:])):
         b1 = box_ids[i]
         b2 = box_ids[j]
         diff = difflib.ndiff(b1, b2)
         l_diff = list(diff)
 #        print l_diff
-        for item in l_diff[:]:
-            if re.match('[-+]', item):
-                pass
-            else:
-                l_diff.pop()
-        if len(l_diff)/2 == 2:
+        if mcount(l_diff):
             print 'letters: ', l_diff
-  #      print len(l_diff)/2
 
 
