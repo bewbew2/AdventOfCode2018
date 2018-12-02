369abe0973fccb94bb503d39e6fdb7d3090ea04d
bewbew2 <paul.yaklin@gmail.com>
2018-12-02 17:34:28 -0600

    First upload

--- Star3.py
+++ Star3.py
@@ -1,3 +1,39 @@
+"""
+--- Day 2: Inventory Management System ---
+You stop falling through time, catch your breath, and check the screen on the device. "Destination reached. Current
+Year: 1518. Current Location: North Pole Utility Closet 83N10." You made it! Now, to find those anomalies.
+
+Outside the utility closet, you hear footsteps and a voice. "...I'm not sure either. But now that so many people have
+chimneys, maybe he could sneak in that way?" Another voice responds, "Actually, we've been working on a new kind of suit
+ that would let him fit through tight spaces like that. But, I heard that a few days ago, they lost the prototype
+ fabric, the design plans, everything! Nobody on the team can even seem to remember important details of the project!"
+
+"Wouldn't they have had enough fabric to fill several boxes in the warehouse? They'd be stored together, so the box IDs
+should be similar. Too bad it would take forever to search the warehouse for two similar box IDs..." They walk too far
+away to hear any more.
+
+Late at night, you sneak to the warehouse - who knows what kinds of paradoxes you could cause if you were discovered -
+and use your fancy wrist device to quickly scan every box and produce a list of the likely candidates (your puzzle
+input).
+
+To make sure you didn't miss any, you scan the likely candidate boxes again, counting the number that have an ID
+containing exactly two of any letter and then separately counting those with exactly three of any letter. You can
+multiply those two counts together to get a rudimentary checksum and compare it to what your device predicts.
+
+For example, if you see the following box IDs:
+
+abcdef contains no letters that appear exactly two or three times.
+bababc contains two a and three b, so it counts for both.
+abbcde contains two b, but no letter appears exactly three times.
+abcccd contains three c, but no letter appears exactly two times.
+aabcdd contains two a and two d, but it only counts once.
+abcdee contains two e.
+ababab contains three a and three b, but it only counts once.
+Of these box IDs, four of them contain a letter which appears exactly twice, and three of them contain a letter which
+appears exactly three times. Multiplying these together produces a checksum of 4 * 3 = 12.
+
+What is the checksum for your list of box IDs?
+"""
 
 with open(u'star3_input.txt', 'r') as f:
     box_ids = f.readlines()
@@ -17,7 +53,7 @@ def mcount(_id):
             d.append(n)
         if n == 3:
             t.append(n)
-    return (len(set(d)),len(set(t)))
+    return len(set(d)), len(set(t))
 
 
 def id_sort(_id):
@@ -33,17 +69,5 @@ for box in sorted_ids:
     d_c = d_c + a
     t_c = t_c + b
 
-
 cksum = d_c * t_c
 print d_c, t_c, cksum
-
-"""
-for box_id in sorted_ids:
-    if has_triples(box_id):
-        count_triple += 1
-
-for item in sorted_ids:
-    print item
-
-print count_triple
-"""
--- Star4.py
+++ Star4.py
@@ -1,31 +1,61 @@
+"""
+Confident that your list of box IDs is complete, you're ready to find the boxes full of prototype fabric.
+
+The boxes will have IDs which differ by exactly one character at the same position in both strings. For example, given
+the following box IDs:
+
+abcde
+fghij
+klmno
+pqrst
+fguij
+axcye
+wvxyz
+The IDs abcde and axcye are close, but they differ by two characters (the second and fourth). However, the IDs fghij and
+ fguij differ by exactly one character, the third (h and u). Those must be the correct boxes.
+
+What letters are common between the two correct box IDs? (In the example above, this is found by removing the differing
+character from either ID, producing fgij.)
+
+Your puzzle answer was qysdtrkloagnfozuwujmhrbvx.
+
+Both parts of this puzzle are complete! They provide two gold stars: **
+
+At this point, you should return to your advent calendar and try another puzzle.
+
+If you still want to see it, you can get your puzzle input.
+"""
 import difflib
 import re
+
 with open(u'star3_input.txt', 'r') as f:
     box_ids = f.readlines()
 
 
-def mcount(l_d):
+def m_count(l_d):
+    """Tis the season to take the list results from the handy function ndiff
+    Looks for counts of + and - characters in the diff output which provides the number of mismatched letters between
+    the compared strings * 2
+    Returns True when there is a single place difference
+    """
     rep_count = 0
     for item in l_d:
         if re.match('[-+]', item):
             rep_count += 1
-#    print rep_count
-
-    if rep_count < 5:
-        if rep_count > 0:
-            return True
+    if rep_count/2 == 1:
+        return True
     else:
         return False
 
 
-for i in range(len(box_ids)):
-    for j in range(len(box_ids[i:])):
+# iterates through all the box ids, comparing with all others
+for i in range(len(box_ids[:])):
+    for j in range(i, len(box_ids)):
         b1 = box_ids[i]
         b2 = box_ids[j]
         diff = difflib.ndiff(b1, b2)
         l_diff = list(diff)
-#        print l_diff
-        if mcount(l_diff):
-            print 'letters: ', l_diff
+        if m_count(l_diff):
+            print ''.join([k.strip() for k in l_diff if not re.match('[+-]', k)])
 
 
